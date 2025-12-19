69904d1c349937e49ebe9d3ad2d8e04011204485
Hari.Kishore <kishore111pec@pec.edu>
2025-12-19 21:09:08 +0530

    add andoid run config

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // ensure uploads folder exists
 const UPLOAD_DIR = process.env.ANDROID ? `${process.env.HOME}/storage/downloads/hari-termux/uploads` : path.join(__dirname, "uploads");
